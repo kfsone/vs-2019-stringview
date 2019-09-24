628fc199fd3537fa07b49e01f7eaa8546161d2d1
Oliver Smith <oliver@kfs.org>
2019-09-24 10:36:06 -0700

    16.4.0p1 error list issue demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,7 @@
 #include "main.h"
 
-#include <iostream>
-#include <string_view>
-
-void output(std::string_view str)
-{
-	std::cout << str << "\n";
-}
-
-// Problem also applies to constexpr lambdas:
-
-constexpr auto greeting = []() {
-	output("Hello, world!\n");
-};
-
 int main()
 {
+	i_err therefore_i_r;
 	greeting();
 }
